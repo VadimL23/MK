b2d1b9edc155bef7dd01f71c536712334ed91644
Vadim <Vadim_lachkov@mail.ru>
2021-04-06 01:36:51 +0700

    add webpack to the game

---
+++ src/main.ts
@@ -0,0 +1,3 @@
+import "./style.css";
+
+alert("Start the game!");
---
+++ webpack.config.prod.js
@@ -0,0 +1,102 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+  entry: './src/main.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle-[hash].js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  devServer: {
+    contentBase: 'dist',
+    compress: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+	      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+	     {
+         test: /\.css$/i,
+         use: ['style-loader', 'css-loader'],
+       }, 
+      {
+        test: /\.(sa|sc)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+     
+    {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+        
+       {
+         test: /\.(svg|woff|woff2|ttf|eot|otf)([\?]?.*)$/,
+         use: [
+           {
+             loader: 'file-loader?name=assets/fonts/[name].[ext]',
+           },
+         ],
+       },
+	    
+	{
+        test: /\.ts$/,
+        enforce: 'pre',
+        loader: 'ts-loader',
+        options: {
+          configFile: 'tsconfig.prod.json'
+        }
+      } 
+	   
+    ],
+         
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: '**/*',
+          context: path.resolve(__dirname, 'src', 'assets'),
+          to: './assets',
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style-[hash].css',
+    }),
+  ],
+};
+
+
