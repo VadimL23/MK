4064263db37ea3d763821958f165abc7eee4933a
Vadim <Vadim_lachkov@mail.ru>
2021-04-14 02:58:53 +0700

    workhome-4

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -1,10 +1,62 @@
 
-const $ = (selector)=>{return document.querySelector(selector)}
-const createElement = (el)=>{return document.createElement(el);}
-const a = ()=>{alert('hi')}
+const setPlayerWidth = (selector,width) =>{
+    selector.querySelector('.life').style.width = width + '%';
+}
+
+   const $ = (selector)=>{return document.querySelector(selector)}
+
+    const createElement = (el, className)=>{
+    const E = document.createElement(el);
+    className && E.classList.add(className);
+    return E;
+    }
+
+
+//const changeHP = (selector,playerObj)=>{
+//    
+//  playerObj.hp -= Math.ceil(Math.random()*20);
+//  if (playerObj.hp <= 0) { playerObj.hp=0; setPlayerWidth(selector,0);return 0;}else{setPlayerWidth(selector,playerObj.hp);return playerObj.hp;};
+//  }
+
+
+
+
+const playerWin = (name) =>{
+    const $winTitle = createElement("div","loseTitle");
+   name !== undefined ?   $winTitle.innerHTML = `${name}`+ " win...": $winTitle.innerHTML ="draw";
+    return $winTitle;
+}
+
+
+function changeHP(val){
+    this.hp=this.hp - val;
+    if (this.hp <=0 ) {this.hp=0;return 0;} else{return this.hp;}
+};
+
+
  
+function elHP(){
+    return this.player;
+}
+
+function renderHP(selector){
+ selector.querySelector('.life').style.width = this.hp + '%';
+};
+
+function createReloadButton(){
+    $reloadWrap = createElement("div","reloadWrap");
+     $reloadButton = createElement("button","button");
+    $reloadWrap.innerHtml=$reloadButton;
+}
+
+
+
  export {
-    $,
-    createElement,
-    a
+        $,
+        createElement,
+        changeHP,
+        playerWin,
+        elHP,
+        renderHP,
+        createReloadButton
  }
--- src/main.js
+++ src/main.js
@@ -1,28 +1,36 @@
 import './style.css';
-import {$,createElement,a}  from './functions'
+import {$,createElement,changeHP,playerWin,elHP,renderHP}  from './functions'
 import heroes  from './store'
 
+
+const player1 = heroes[Math.floor(Math.random()*5)];
+player1.player = 1;
+
+const player2 = heroes[Math.floor(Math.random()*5)];
+player2.player = 2;
+
+
+
+
 const $arenas  = $('.arenas');
+const $randomButton = $('#button');
 
 const createPlayer = (name,heroObj)=>{
 try{
-    const $playerDiv = createElement('div');
-    $playerDiv.classList.add(`${name}`);
+    const $playerDiv = createElement('div',`${name}`);
+
        
-    const $progressbar = createElement('div');
-    $progressbar.classList.add(`progressbar`);
+    const $progressbar = createElement('div',`progressbar`);
+ 
     
-    const $life = createElement('div');
-    $life.classList.add(`life`);
+    const $life = createElement('div',`life`);
     $life.style.width=`${heroObj.hp}%`;
     
-    const $name = createElement('div');
-    $name.classList.add(`name`);
+    const $name = createElement('div',`name`);
     $name.innerHTML = `${heroObj.name}`;
     
-    const $character = createElement('div');
-    $character.classList.add(`character`);
-    
+    const $character = createElement('div',`character`);
+   
     const $img = createElement('img');
     $img.src=heroObj.img;
     
@@ -34,8 +42,8 @@ try{
     $playerDiv.appendChild($progressbar);
     $playerDiv.appendChild($character);
     
-    $arenas.appendChild($playerDiv);
-    return 1;
+
+    return $playerDiv;
 }
     catch(err){
         console.log("Err create player ",err);
@@ -43,6 +51,43 @@ try{
     
 }
 
-createPlayer('player1',heroes[0]);
 
-createPlayer('player2',heroes[1]);
+$randomButton.addEventListener('click',()=>{
+   
+  changeHP.call(player1,Math.ceil(Math.random()*20));
+  changeHP.call(player2,Math.ceil(Math.random()*20));
+  renderHP.call(player1,$player1);
+  renderHP.call(player2,$player2);
+    
+if (player1.hp == 0 || player2.hp == 0 ){
+     $randomButton.disabled = true;  
+  }
+    
+if  (player1.hp === 0 && player1.hp< player2.hp){
+      
+       $arenas.appendChild(playerWin(player2.name));
+ 
+  }
+    else if ( player2.hp === 0 && player2.hp< player1.hp){
+        
+       $arenas.appendChild(playerWin(player1.name));
+  
+    }
+    
+    else if (player1.hp === 0 &&  player2.hp === 0){
+        
+        playerWin();
+    }
+    
+
+    
+    
+});
+    
+const $player1 = createPlayer('player1',player1);
+const $player2 = createPlayer('player2',player2);
+
+
+$arenas.appendChild($player1);
+
+$arenas.appendChild($player2);
